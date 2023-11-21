2d140849d1adecf76e3d6d8e0c4a1499bf700888
BioGavin <gavinchou99@126.com>
2023-11-21 21:30:59 +0800

    code upgrade

--- 16S_MAG_linker.py
+++ 16S_MAG_linker.py
@@ -7,18 +7,6 @@ import queue
 import argparse
 import util.output as output
 
-parser = argparse.ArgumentParser(description='Calculate connection between 16S and MAG')
-
-parser.add_argument('-t', '--thread', metavar='', type=int, help='Num of threads')
-
-parser.add_argument('-r', '--rna', metavar='', type=str, help='Num of rna')
-
-parser.add_argument('-g', '--genome', metavar='', type=str, help='Num of genome')
-
-parser.set_defaults(thread=8)
-
-args = parser.parse_args()
-
 
 def work(q, dic_16s, dic_mag, matchDic, diclock, pbar):
     while True:
@@ -30,86 +18,86 @@ def work(q, dic_16s, dic_mag, matchDic, diclock, pbar):
             util.genome_match(dic_16s, dic_mag, do, matchDic, diclock)
 
 
-folder_16s = './16s'
+def write_json(file_name, data):
+    with open(file_name, 'w') as f:
+        json.dump(data, f)
 
-temp_16s = util.get_filelist(folder_16s, [])
-list_16s = []
 
-count = 0
+def help():
+    parser = argparse.ArgumentParser(description='Calculate connection between 16S and MAG')
 
-for e in temp_16s:
-    if e.endswith('.fasta') or e.endswith('.fna'):
-        list_16s.append(e)
-        count += 1
+    parser.add_argument('-t', '--thread', metavar='', type=int, help='Num of threads')
 
-folder_mag = './mag'
+    parser.add_argument('-r', '--rna', metavar='', type=str, help='Num of rna')
 
-temp_mag = util.get_filelist(folder_mag, [])
+    parser.add_argument('-g', '--genome', metavar='', type=str, help='Num of genome')
 
-list_mag = []
+    parser.set_defaults(thread=8)
 
-Tcount = 0
+    args = parser.parse_args()
 
-for e in temp_mag:
+    return args
 
-    if e.endswith('.fna.gz') or e.endswith('.fna') or e.endswith('.fa'):
-        list_mag.append(e)
-        Tcount += 1
 
-with open('magspath.txt', 'w') as f:
-    for i in list_mag:
-        f.write(i + '\n')
+if __name__ == '__main__':
+    args = help()
 
-count = 0
+    folder_16s = args.rna
+    temp_16s = util.get_filelist(folder_16s, [])
+    list_16s = []
+    for e in temp_16s:
+        if e.endswith('.fasta') or e.endswith('.fna'):
+            list_16s.append(e)
 
-dic_16s = {}
-dic_mag = {}
+    folder_mag = args.genome
+    temp_mag = util.get_filelist(folder_mag, [])
+    list_mag = []
+    for e in temp_mag:
+        if e.endswith('.fna.gz') or e.endswith('.fna') or e.endswith('.fa'):
+            list_mag.append(e)
 
-diclock = Lock()
+    with open('magspath.txt', 'w') as f:
+        for i in list_mag:
+            f.write(i + '\n')
 
-q = queue.Queue()
+    dic_16s, dic_mag = {}, {}
 
-if not os.path.exists('./matchData'):
-    os.mkdir('./matchData')
+    diclock = Lock()
+    q = queue.Queue()
 
-if not os.path.exists('./resultData'):
-    os.mkdir('./resultData')
+    if not os.path.exists('./matchData'):
+        os.mkdir('./matchData')
 
-for seg in tqdm(list_16s):
-    print(seg)
-    count += 1
-    print('Start %d batch of 16S rRNA analysis' % (count))
-    util.blast_16S(seg)
-    util.blast_filter('blastResult.txt')
+    if not os.path.exists('./resultData'):
+        os.mkdir('./resultData')
 
-    with open('matchdic.json', 'r') as f:
-        matchDic = json.load(f)
+    for seg in tqdm(list_16s):
+        print(seg)
+        util.blast_16S(seg)
+        util.blast_filter('blastResult.txt')
 
-    for match in matchDic:
-        print(match)
-        q.put(match)
+        with open('matchdic.json', 'r') as f:
+            matchDic = json.load(f)
 
-    pbar = tqdm(total=len(matchDic))
-    print('OK')
-    thread_num = args.thread
-    threads = []
-    for i in range(thread_num):
-        t = Thread(target=work, args=(q, dic_16s, dic_mag, matchDic, diclock, pbar))
-        # args需要输出的是一个元组，如果只有一个参数，后面加，表示元组，否则会报错
-        threads.append(t)
-    # 创建5个线程
-    for i in range(thread_num):
-        threads[i].start()
-    for i in range(thread_num):
-        threads[i].join()
+        for match in matchDic:
+            print(match)
+            q.put(match)
 
-with open('16s.json', 'w') as f:
-    json.dump(dic_16s, f)
+        pbar = tqdm(total=len(matchDic))
+        thread_num = args.thread
+        threads = []
+        for i in range(thread_num):
+            t = Thread(target=work, args=(q, dic_16s, dic_mag, matchDic, diclock, pbar))
+            # args需要输出的是一个元组，如果只有一个参数，后面加，表示元组，否则会报错
+            threads.append(t)
+        # 创建5个线程
+        for i in range(thread_num):
+            threads[i].start()
+        for i in range(thread_num):
+            threads[i].join()
 
-with open('mag.json', 'w') as f:
-    json.dump(dic_mag, f)
+    write_json('16s.json', dic_16s)
+    write_json('mag.json', dic_mag)
 
-output.makexlsx('16s.xlsx', dic_16s, ['16S', 'MAG', 'valuation'])
-output.makexlsx('mag.xlsx', dic_mag, ['MAG', '16S', 'valuation'])
-
-print('Analysis Complete! Please check 16s.xlsx and mag.xlsx to get more infomation.')
+    output.makexlsx('16s.xlsx', dic_16s, ['16S', 'MAG', 'valuation'])
+    output.makexlsx('mag.xlsx', dic_mag, ['MAG', '16S', 'valuation'])
